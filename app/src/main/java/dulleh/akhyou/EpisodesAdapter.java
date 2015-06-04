b125c50d119c787c5a587a35f398f35f21dab63d
dulleh <dully101@hotmail.co.uk>
2015-06-04 18:06:41 +0100

    c

---
+++ app/src/main/java/dulleh/akhyou/EpisodesAdapter.java
@@ -0,0 +1,50 @@
+package dulleh.akhyou;
+
+import android.content.Context;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class EpisodesAdapter extends ArrayAdapter<EpisodeObject> {
+    private final Context context;
+
+    static class ViewHolderItem {
+        TextView textView;
+    }
+
+    public EpisodesAdapter(Context context, ArrayList<EpisodeObject> arrayList) {
+
+        super(context, R.layout.episode_item, arrayList);
+        this.context = context;
+
+    }
+
+    @Override
+    public View getView(int position, View rowView, ViewGroup parent) {
+
+        ViewHolderItem viewHolder;
+
+        if (rowView == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            rowView = inflater.inflate(R.layout.episode_item, parent, false);
+
+            viewHolder = new ViewHolderItem();
+            viewHolder.textView = (TextView) rowView.findViewById(R.id.episodeTitleTV);
+
+            rowView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolderItem) rowView.getTag();
+        }
+
+        viewHolder.textView.setText(getItem(position).getEpisodeTitle());
+
+        return rowView;
+
+    }
+
+}
